048e98ea99886445b5541499aa2309e5f7e9baee
Yeboka <89529214+Yeboka@users.noreply.github.com>
2022-12-15 22:21:35 +0600

    creating tables

--- Triggers/gpaEvaluate.sql
+++ Triggers/gpaEvaluate.sql
@@ -1,1 +1,9 @@
-drop trigger gpaEvaluate
+
+create trigger gpaEvaluate
+    after update on Enroll
+    when (old.student_id = new.student_id and old.section_id = new.section_id and old.grade <> new.grade)
+    begin
+        update Students set GPA = (((select sum(grade) from Enroll where Enroll.student_id = new.student_id)
+                                       / (select count(grade) from Enroll where Enroll.student_id = new.student_id)) * 4 / 100 )
+                        where Students.student_id = new.student_id;
+    end;
